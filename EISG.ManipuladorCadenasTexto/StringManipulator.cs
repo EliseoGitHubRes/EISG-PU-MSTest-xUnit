469eb7d2b525d5d3a8d6117f76970c69540ae4a1
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-10-11 12:22:40 -0600

    Añadir nuevo proyecto EISG.ManipuladorCadenasTexto
    Se ha añadido un nuevo proyecto `EISG.ManipuladorCadenasTexto` al archivo de solución `EISG.sln` con el identificador `{69AA6849-E6D5-4E64-AB92-DF116CA64B3A}`.
    
    Detalles del proyecto:
    - Archivo de proyecto `EISG.ManipuladorCadenasTexto.csproj`:
      - SDK: `Microsoft.NET.Sdk`
      - Marco de destino: `net6.0`
      - `ImplicitUsings` habilitado
      - Opción `Nullable` habilitada
    
    Se ha añadido la clase `StringManipulator` con los métodos:
    - `ReverseString`: Invierte una cadena de texto.
    - `RemoveSpaces`: Elimina todos los espacios en blanco de una cadena de texto.
    
    Configuraciones de compilación añadidas para los modos `Debug` y `Release`.

---
+++ EISG.ManipuladorCadenasTexto/StringManipulator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EISG.ManipuladorCadenasTexto
+{
+	public class StringManipulator
+	{
+		// Método que invierte la cadena de texto
+		public string ReverseString(string input)
+		{
+			// Convertir la cadena a un array de caracteres, invertir el array y devolverlo como string
+			char[] charArray = input.ToCharArray();
+			Array.Reverse(charArray);
+			return new string(charArray);
+		}
+
+		// Método que elimina todos los espacios en blanco de la cadena
+		public string RemoveSpaces(string input)
+		{
+			// Eliminar todos los espacios de la cadena usando Replace
+			return input.Replace(" ", "");
+		}
+	}
+}
